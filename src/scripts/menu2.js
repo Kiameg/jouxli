279c665dbfeb4edac0b867072b8681053a4a8484
Kiameg <kiax1262@stud.kea.dk>
2023-05-24 00:47:02 +0200

    kontakt

---
+++ src/scripts/menu2.js
@@ -0,0 +1,21 @@
+const menuButton = document.querySelector("button");
+const menuElement = document.querySelector(".menu");
+const header = document.querySelector("header");
+let isMenuOpen = false;
+
+menuButton.addEventListener("click", showMenu);
+
+function showMenu() {
+  //Funktion til at åbne menu
+  if (isMenuOpen) {
+    // Hvis menuen er åben
+    menuButton.textContent = "MENU";
+    isMenuOpen = false;
+    header.style.backgroundColor = "rgba(242, 229, 213, 0)";
+  } else {
+    menuButton.textContent = "LUK";
+    isMenuOpen = true;
+    header.style.backgroundColor = "rgba(242, 229, 213, 0.75)";
+  }
+  menuElement.classList.toggle("menu_active");
+}
