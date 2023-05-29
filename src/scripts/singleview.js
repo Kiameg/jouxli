f5a47d573b4cbac2e4423f0d22adde326dd6267d
Misha Danneværn Marker <mish0144@stud.kea.dk>
2023-05-30 00:36:47 +0200

    rettelser, baggrunde, nye billeder

--- src/scripts/singleview.js
+++ src/scripts/singleview.js
@@ -1,9 +1,8 @@
+const miniPhotos = document.querySelectorAll(".mini-photo")
+const bigPhoto = document.querySelector(".img_big")
 
-
-
-console.log("Hello world!");
-
-
-
-
-
+miniPhotos.forEach(el => el.addEventListener("click", function(){
+    miniPhotos.forEach(el => el.classList.add("fade"))
+    this.classList.remove("fade")
+    bigPhoto.src = this.src
+}));
